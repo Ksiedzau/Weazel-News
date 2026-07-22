2b13f3f93e1cf261856f099f91be7ed8e9c78e21
Ksiedzau <Tkfans231@gmail.com>
2026-07-22 23:38:40 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // WPISZ TUTAJ SWOJE DANE Z SUPABASE:
-const SUPABASE_URL = 'TUTAJ_TWÓJ_SUPABASE_URL';
-const SUPABASE_KEY = 'TUTAJ_TWÓJ_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
