a506876cc11f4196311aa6b9586204a047d538c3
Ksiedzau <Tkfans231@gmail.com>
2026-07-22 23:41:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
 // --- LISTY ID Z DISCORDA ---
 // Wklej odpowiednie ID użytkowników z Discorda:
-const BOSS_DISCORD_IDS = ['TWOJE_DISCORD_ID']; 
+const BOSS_DISCORD_IDS = ['1487414745534431233']; 
 const ADMIN_DISCORD_IDS = ['TWOJE_DISCORD_ID'];
 const CITY_HALL_DISCORD_IDS = ['ID_OSOBY_Z_CITY_HALL']; // Ranga City Hall
