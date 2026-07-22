bfcade7ae72a8a3344cfd9cd587303ba8b189f78
Ksiedzau <Tkfans231@gmail.com>
2026-07-22 23:00:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+// --- KONFIGURACJA SUPABASE ---
 const SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
-// Wklej tutaj swoje ID z Discorda (pamiętaj o cudzysłowach!):
-const BOSS_DISCORD_IDS = ['1487414745534431233']; 
-const ADMIN_DISCORD_IDS = ['TWOJE_DISCORD_ID_TUTAJ'];
+// --- LISTY ID Z DISCORDA ---
+// Wklej odpowiednie ID użytkowników z Discorda:
+const BOSS_DISCORD_IDS = ['TWOJE_DISCORD_ID']; 
+const ADMIN_DISCORD_IDS = ['TWOJE_DISCORD_ID'];
+const CITY_HALL_DISCORD_IDS = ['ID_OSOBY_Z_CITY_HALL']; // Ranga City Hall
