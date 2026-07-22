8cc795c472fdca9150c7527c9439c0c98a538171
Ksiedzau <Tkfans231@gmail.com>
2026-07-22 22:49:12 +0200

    Create config.js with Supabase and Discord settings
    Add configuration for Supabase and Discord IDs

---
+++ config.js
@@ -0,0 +1,10 @@
+// --- KONFIGURACJA SUPABASE ---
+const SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
+
+// --- TWOJE LISTY ID Z DISCORDA ---
+// Tutaj wklej swoje ID użytkownika na Discordzie
+const BOSS_DISCORD_IDS = ['TUTAJ_WPISZ_SWOJE_DISCORD_ID']; 
+
+// Tutaj wpisz ID swoje oraz ewentualnych innych administratorów
+const ADMIN_DISCORD_IDS = ['TUTAJ_WPISZ_SWOJE_DISCORD_ID', 'ID_INNEGO_ADMINA'];
