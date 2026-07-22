ad23f4338f4bcffb27d66a497204d789624a27ee
Ksiedzau <Tkfans231@gmail.com>
2026-07-22 22:52:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
 // --- TWOJE LISTY ID Z DISCORDA ---
 // Tutaj wklej swoje ID użytkownika na Discordzie
-const BOSS_DISCORD_IDS = ['TUTAJ_WPISZ_SWOJE_DISCORD_ID']; 
+const BOSS_DISCORD_IDS = ['1487414745534431233']; 
 
 // Tutaj wpisz ID swoje oraz ewentualnych innych administratorów
 const ADMIN_DISCORD_IDS = ['TUTAJ_WPISZ_SWOJE_DISCORD_ID', 'ID_INNEGO_ADMINA'];
