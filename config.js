f2c2e18ff2041abc16a4a4d94e36c73c2c15e5bb
Ksiedzau <Tkfans231@gmail.com>
2026-07-22 23:44:08 +0200

    Uncomment CITY_HALL_DISCORD_IDS assignment

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ window.SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
 window.SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
 // --- LISTY ID Z DISCORDA ---
+// Wklej odpowiednie ID użytkowników z Discorda w cudzysłowach:
 window.BOSS_DISCORD_IDS = ['1487414745534431233']; 
 window.ADMIN_DISCORD_IDS = ['TWOJE_DISCORD_ID'];
-window.CITY_HALL_DISCORD_IDS = ['ID_OSOBY_Z_CITY_HALL'];
+window.CITY_HALL_DISCORD_IDS = ['ID_OSOBY_Z_CITY_HALL']; // ID osób z rangą City Hall
