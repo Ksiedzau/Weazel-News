6d3eab41ca1ec506f13eb4c2b81f31fc0e9e22fa
Ksiedzau <Tkfans231@gmail.com>
2026-07-23 13:24:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,59 @@
-// --- KONFIGURACJA SUPABASE ---
-window.SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
-window.SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
+// ===============================
+// KONFIGURACJA SUPABASE
+// ===============================
 
-// --- LISTY ID Z DISCORDA ---
+window.SUPABASE_URL =
+    'https://mwymbvvlxcnmqtvdewgh.supabase.co';
+
+window.SUPABASE_KEY =
+    'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
+
+
+// ===============================
+// RANGA SZEF
+// ===============================
+
 window.BOSS_DISCORD_IDS = [
     '1487414745534431233',
     '1183805781724893245'
 ];
 
+
+// ===============================
+// ADMINI
+// ===============================
+
 window.ADMIN_DISCORD_IDS = [
     '1391147972024078519',
     '1227230815994515517',
-    '1456955414674407515',
-    'TUTAJ_ID_ADMINA_3'
+    '1456955414674407515'
 ];
+
+
+// ===============================
+// CITY HALL
+// ===============================
 
 window.CITY_HALL_DISCORD_IDS = [
-    '1007988667077369857',
-    'TUTAJ_ID_URZEDNIKA_1',
-    'TUTAJ_ID_URZEDNIKA_2'
+    '1007988667077369857'
 ];
 
-// FIRMY - mogą dodawać ogłoszenia tylko w kategorii "Ogłoszenia firmy"
+
+// ===============================
+// FIRMY
+// Na razie puste.
+// Gdy podasz ID firm, wpisz je tutaj.
+// ===============================
+
 window.COMPANY_DISCORD_IDS = [
-    // 'ID_DISCORD_FIRMY_1',
-    // 'ID_DISCORD_FIRMY_2'
+    // 'ID_DISCORDA_OSOBY_Z_FIRMY_1',
+    // 'ID_DISCORDA_OSOBY_Z_FIRMY_2'
 ];
 
-window.RECRUIT_DISCORD_URL = 'https://discord.gg/yQDZNv9heE';
+
+// ===============================
+// DISCORD REKRUTACYJNY
+// ===============================
+
+window.RECRUIT_DISCORD_URL =
+    'https://discord.gg/yQDZNv9heE';
