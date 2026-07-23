37dcaca1221a0257f8d59b92d0d17d375954b100
Ksiedzau <Tkfans231@gmail.com>
2026-07-23 11:33:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
-// --- KONFIGURACJA SUPABASE ---
+// config.js
+
 window.SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
 window.SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
-// --- LISTY ID Z DISCORDA ---
-// Wklej odpowiednie ID użytkowników z Discorda w cudzysłowach:
-window.BOSS_DISCORD_IDS = ['1487414745534431233']; 
-window.ADMIN_DISCORD_IDS = ['TWOJE_DISCORD_ID'];
-window.CITY_HALL_DISCORD_IDS = ['1487414745534431233']; // ID osób z rangą City Hall
+// Szefowie - pełny dostęp do Panelu Admina
+window.BOSS_DISCORD_IDS = [
+    '1487414745534431233'
+];
+
+// Administratorzy - pełny dostęp do Panelu Admina
+window.ADMIN_DISCORD_IDS = [
+    // 'TU_WPISZ_KOLEJNE_ID'
+];
+
+// Osoby City Hall - mogą dodawać tylko wpisy do City Hall
+window.CITY_HALL_DISCORD_IDS = [
+    '1487414745534431233'
+];
