4eb2b230dc49ab26c1a98d843d92f6284bc8d00f
Ksiedzau <Tkfans231@gmail.com>
2026-07-23 16:12:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,36 @@
+// =====================================================
+// WEAZEL NEWS - KONFIGURACJA
+// =====================================================
+
 window.SUPABASE_URL =
     "https://mwymbvvlxcnmqtvdewgh.supabase.co";
 
 window.SUPABASE_KEY =
     "sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN";
 
+// Stały adres strony GitHub Pages.
+// Dzięki temu OAuth zawsze wraca w dobre miejsce.
+window.SITE_URL =
+    "https://ksiedzau.github.io/Weazel-News/";
+
+// SZEFOWIE
 window.BOSS_DISCORD_IDS = [
     "1487414745534431233",
     "1183805781724893245"
 ];
 
+// ADMINI
 window.ADMIN_DISCORD_IDS = [
     "1391147972024078519",
     "1227230815994515517",
     "1456955414674407515"
 ];
 
+// CITY HALL
 window.CITY_HALL_DISCORD_IDS = [
     "1007988667077369857"
 ];
 
+// DISCORD DO REKRUTACJI
 window.RECRUIT_DISCORD_URL =
     "https://discord.gg/yQDZNv9heE";
