7f21b123f66d563183d497a4a8b5b09a88e05f3a
Ksiedzau <Tkfans231@gmail.com>
2026-07-23 11:40:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ window.SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 // --- LISTY ID Z DISCORDA ---
 window.BOSS_DISCORD_IDS = ['1487414745534431233'];
 
-// Panel admina ma być dla ADMIN_DISCORD_IDS
-window.ADMIN_DISCORD_IDS = ['1487414745534431233']; // <-- ZMIEŃ na swoje ID
+// Kto widzi Panel Admina i może dodawać wpisy
+window.ADMIN_DISCORD_IDS = ['1487414745534431233'];
 
-window.CITY_HALL_DISCORD_IDS = ['1487414745534431233']; // <-- jeśli używasz do filtrowania
+// Kto może dodawać ogłoszenia w kategorii CITY HALL
+window.CITY_HALL_DISCORD_IDS = ['1487414745534431233'];
+
+// Link do Discorda rekrutacyjnego
+window.RECRUIT_DISCORD_URL = 'https://discord.gg/yQDZNv9heE';
