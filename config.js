ec11dd43fbd220da7c073698f004ebcddbc576a2
Ksiedzau <Tkfans231@gmail.com>
2026-07-23 12:35:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,28 @@ window.SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
 window.SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
 // --- LISTY ID Z DISCORDA ---
-window.BOSS_DISCORD_IDS = ['1487414745534431233'];
+// Każde ID w cudzysłowie, oddzielone przecinkiem.
 
-// Kto widzi Panel Admina i może dodawać wpisy
-window.ADMIN_DISCORD_IDS = [''];
+// SZEF (właściciel / naczelny) - widzi Panel Admina + ranga "Szef"
+window.BOSS_DISCORD_IDS = [
+    '1487414745534431233',
+    '1183805781724893245'
+];
 
-// Kto może dodawać ogłoszenia w kategorii CITY HALL
-window.CITY_HALL_DISCORD_IDS = [''];
+// ADMIN - widzi Panel Admina + ranga "Admin"
+window.ADMIN_DISCORD_IDS = [
+    '1391147972024078519',
+    '1227230815994515517',
+    '1456955414674407515',
+    'TUTAJ_ID_ADMINA_3'
+];
+
+// CITY HALL - może publikować w kategorii City Hall + ranga "City Hall"
+window.CITY_HALL_DISCORD_IDS = [
+    '1007988667077369857',
+    'TUTAJ_ID_URZEDNIKA_1',
+    'TUTAJ_ID_URZEDNIKA_2'
+];
 
 // Link do Discorda rekrutacyjnego
 window.RECRUIT_DISCORD_URL = 'https://discord.gg/yQDZNv9heE';
