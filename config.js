569878c89b7461281044354bfec303edac461f47
Ksiedzau <Tkfans231@gmail.com>
2026-07-23 12:52:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,11 @@ window.SUPABASE_URL = 'https://mwymbvvlxcnmqtvdewgh.supabase.co';
 window.SUPABASE_KEY = 'sb_publishable_ih2IDk7NUpRav8RC-pVHdg_HRdb2vyN';
 
 // --- LISTY ID Z DISCORDA ---
-// Każde ID w cudzysłowie, oddzielone przecinkiem.
-
-// SZEF (właściciel / naczelny) - widzi Panel Admina + ranga "Szef"
 window.BOSS_DISCORD_IDS = [
     '1487414745534431233',
     '1183805781724893245'
 ];
 
-// ADMIN - widzi Panel Admina + ranga "Admin"
 window.ADMIN_DISCORD_IDS = [
     '1391147972024078519',
     '1227230815994515517',
@@ -19,12 +15,16 @@ window.ADMIN_DISCORD_IDS = [
     'TUTAJ_ID_ADMINA_3'
 ];
 
-// CITY HALL - może publikować w kategorii City Hall + ranga "City Hall"
 window.CITY_HALL_DISCORD_IDS = [
     '1007988667077369857',
     'TUTAJ_ID_URZEDNIKA_1',
     'TUTAJ_ID_URZEDNIKA_2'
 ];
 
-// Link do Discorda rekrutacyjnego
+// FIRMY - mogą dodawać ogłoszenia tylko w kategorii "Ogłoszenia firmy"
+window.COMPANY_DISCORD_IDS = [
+    // 'ID_DISCORD_FIRMY_1',
+    // 'ID_DISCORD_FIRMY_2'
+];
+
 window.RECRUIT_DISCORD_URL = 'https://discord.gg/yQDZNv9heE';
